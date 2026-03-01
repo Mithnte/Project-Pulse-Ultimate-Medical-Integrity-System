2e0d3b4e5dbbc190f7af60a64fdbbb5a4f6d95ca
Mithnte <baehareq@gmail.com>
2026-03-01 14:31:00 +0900

    Create fxmanifest.lua for medical overhaul project
    Added fxmanifest for Advanced Clinical Medical Overhaul.

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Vibe Dev x IMER'
+description 'Advanced Clinical Medical Overhaul for 1.9k Players'
+
+shared_script '@ox_lib/init.lua' -- Wajib terinstall ox_lib
+
+client_scripts { 'client.lua' }
+server_scripts { 'server.lua' }
+
+ui_page 'html/index.html'
+files { 'html/index.html' }
